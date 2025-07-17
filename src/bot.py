dc73317f3ad8c3cfa3f7e0f73c372ca3774ea9b5
stephenhky <stephenhky@yahoo.com.hk>
2025-07-17 17:50:21 -0400

    bot reply handling

--- src/bot.py
+++ src/bot.py
@@ -1,8 +1,17 @@
 
 import os
+import sys
 
 import telebot
+from dotenv import load_dotenv
+from time import time
+from langchain_core.messages import AIMessage
 
+from .agents.graph import FinportAgent
+from .agents.aws import get_aws_bedrock_llm
+
+
+load_dotenv()
 
 # It's a good practice to get the bot token from an environment variable.
 telegram_api_key = os.environ.get("API_KEY")
@@ -10,5 +19,31 @@ bot = telebot.TeleBot(telegram_api_key, threaded=False)
 
 
 @bot.message_handler(func=lambda message: True)
-def process_user_input_with_agent(message):
-    pass
+def process_user_input_with_agent(message: telebot.types.Message):
+    # initialize agent
+    llm_id = os.environ.get('BEDROCK_LLM_ID')
+    llm = get_aws_bedrock_llm(llm_id)
+    agent = FinportAgent(llm)
+
+    # process message
+    input_text = message.text
+    starttime = time()
+    # TODO: make it to reply
+    for value in agent.stream_graph_updates(input_text):
+        print("======================", file=sys.stderr)
+        current_time = time()
+        print(f"Time elapsed: {current_time - starttime} sec", file=sys.stderr)
+        print(value, file=sys.stderr)
+        if 'messages' in value:
+            for message in value['messages']:
+                print(message, file=sys.stderr)
+                print(type(message), file=sys.stderr)
+                if isinstance(message, AIMessage):
+                    if isinstance(message.content, str):
+                        yield message.content
+                    elif isinstance(message.content, list):
+                        for content in message.content:
+                            if content['type'] == 'text':
+                                yield "-->" + content['text']
+                            elif content['type'] == 'image_url':
+                                yield f"Image URL: {content['image_url']}"
